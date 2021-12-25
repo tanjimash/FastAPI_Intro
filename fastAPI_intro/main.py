eb4d27bfb38eb128358df8d6bf0cb92f6d85cd0f
Ashraf840 <shift2unleashed2011@gmail.com>
2021-12-25 21:37:56 +0600

    passing dynamic-routing-variables in the path-string

--- fastAPI_intro/main.py
+++ fastAPI_intro/main.py
@@ -9,7 +9,7 @@ app = FastAPI()
 @app.get( '/' )
 def index():
     context = {
-        'title': 'Homepage',
+        'title': 'Homepage API',
     }
     return { 'data': context }
 
@@ -19,6 +19,51 @@ def index():
 @app.get( '/about/' )
 def about():
     context = {
-        'title': 'About Page',
+        'title': 'About Page API',
     }
     return { 'data': context }
+
+
+
+
+@app.get( '/blog/' )
+def blogList():
+    context = {
+        'title': 'Blog List API',
+    }
+    return { 'data': context }
+
+
+
+
+# [NB]: for dynamic routing, use any routing-variable inside the second-bracket "{}"
+#   To pass the routing0variable inside the function, use func-params.
+@app.get( '/blog/{id}/' )
+def blogDetail(id):
+    context = {
+        'title': 'Blog Detail API',
+        'Blog ID': id,
+    }
+    return { 'data': context }
+
+
+
+
+# blog-comments
+@app.get( '/blog/{id}/comments/' )
+def blogComments(id):
+    comments = { '1','2', '3', '4', '5' }
+    context = {
+        'title': 'Blog Detail - Comments API',
+        'Blog ID': id,
+        'Blog Comments': comments,
+    }
+    return { 'data': context }
+
+
+
+
+
+
+
+
