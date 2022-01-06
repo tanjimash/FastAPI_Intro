c198eab611a5401938cbedb745ba538c1263b1fe
tanjimash <tanjimashraf.cit.bd@gmail.com>
2022-01-06 11:56:26 +0600

    Complete the CRUD of 'User' model

--- blog/main.py
+++ blog/main.py
@@ -236,3 +236,63 @@ def get_individual_user_detail( id, response: Response, db: Session = Depends( g
         )
     return user
 
+
+
+
+# Update a specific user
+@app.put( 
+    '/subfolder/user/{id}', 
+    status_code=status.HTTP_202_ACCEPTED, 
+    tags=["Users"] )
+# Make a request-body for the client (schemas) & fetch the db-session-model instance
+def update_blog( id, request: schemas.User, db: Session = Depends( get_db ) ):
+    user = db.query( models.User ).filter( models.User.id == id ).first()
+    
+    # Raise exception: if no such user is available with that ID.
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail= f'No such user with id {id} is available!'
+        )
+
+    # Update the model-instance field using the client's request-body-field (schema - from the browser)
+    user.name = request.name
+    user.email = request.email
+    user.password = request.password
+
+    db.commit()
+    # # after committed updation, refresh the db.
+    db.refresh( user )
+    # # provide the updated user object (fetched from the db again).
+
+    return { 
+        'msg': 'User detail is updated!',
+        'User (Updated)': user,
+    }
+
+
+
+
+
+# Delete a specific user
+# [ NOTE ]: when the status-code 204 is used, nothing will be returned as response
+@app.delete( 
+    '/subfolder/user/{id}', 
+    status_code=status.HTTP_204_NO_CONTENT, 
+    tags=["Users"] )
+# @app.delete( '/subfolder/blog/{id}', status_code=204 )
+# @app.delete( '/subfolder/blog/{id}' )
+def delete_blog( id, db: Session=Depends( get_db ) ):
+    # make a query from the "User" model & filter based on the "ID"
+    # search for the user-obj from the db-model "User"
+    user = db.query( models.User ).filter( models.User.id == id )
+    
+    # Raise exception: if no such blog is available with that ID.
+    if not user.first():
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail= f'No such user with id {id} is available!'
+        )
+    
+    user.delete( synchronize_session=False )
+    db.commit()  
